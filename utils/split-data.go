d2767bdc49e3cf79d09b03087e26edfaeef8fa10
hawkeshz <33495222+hawkeshz@users.noreply.github.com>
2018-06-24 02:39:16 +0500

    Add files via upload

---
+++ utils/split-data.go
@@ -0,0 +1,57 @@
+ package main
+
+ import (
+         "fmt"
+         "io/ioutil"
+         "math"
+         "os"
+         "strconv"
+ )
+
+ func main() {
+
+         fileToBeChunked := "/home/najeeb/Workspace/go/src/github.com/NajeebTyson/cds-project/data/realhuman_phill.txt"
+
+         file, err := os.Open(fileToBeChunked)
+
+         if err != nil {
+                 fmt.Println(err)
+                 os.Exit(1)
+         }
+
+         defer file.Close()
+
+         fileInfo, _ := file.Stat()
+
+         var fileSize int64 = fileInfo.Size()
+
+         const fileChunk = 100 * (1 << 20) // 1 MB, change this to your requirement
+
+         // calculate total number of parts the file will be chunked into
+
+         totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+
+         fmt.Printf("Splitting to %d pieces.\n", totalPartsNum)
+
+         for i := uint64(0); i < totalPartsNum; i++ {
+
+                 partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+                 partBuffer := make([]byte, partSize)
+
+                 file.Read(partBuffer)
+
+                 // write to disk
+                 fileName := "chunk_" + strconv.FormatUint(i, 10) + ".txt"
+                 _, err := os.Create(fileName)
+
+                 if err != nil {
+                         fmt.Println(err)
+                         os.Exit(1)
+                 }
+
+                 // write/save buffer to disk
+                 ioutil.WriteFile(fileName, partBuffer, os.ModeAppend)
+
+                 fmt.Println("Split to : ", fileName)
+         }
+ }
